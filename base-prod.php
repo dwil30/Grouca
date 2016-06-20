b29a059da75bb4007f7cf0437019631f7e21a2a3
Damon Wilson <damon.d.wilson@gmail.com>
2016-06-20 11:10:32 -0700

    Update DB details

--- base-prod.php
+++ base-prod.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbhost = "mysql51-127.wc1.ord1.stabletransit.com"; // this will ususally be 'localhost', but can sometimes differ
+$dbhost = "23.253.3.10"; // this will ususally be 'localhost', but can sometimes differ
 $dbname = "943880_grouca"; // the name of the database that you are going to use for this project
 $dbuser = "943880_grouca"; // the username that you created, or were given, to access your database
 $dbpass = "Sergio123!"; // the password that you created, or were given, to access your database
